a85f400ae0fea3a385e93668036f019f39705fd6
Jonathan Lee <jonlee@westmont.edu>
2020-01-18 16:33:30 -0800

    Fixed alignment issues on work component

--- src/components/work.js
+++ src/components/work.js
@@ -1,32 +1,38 @@
 import PropTypes from "prop-types"
 import React from "react"
-import aboutStyles from "../styles/block.module.css"
 import styled from 'styled-components';
 import { green } from "color-name";
 
-const MenuSection = styled.div`
-    max-width: 90%;
-    width 700%;
-    border-bottom: 10px;
+const Card = styled.div`
+    display: block;
+`
+
+const MenuContainer = styled.div`
+    display: flex;
 `
 
 const MenuBar = styled.ul`
+    display: block;
     list-style-type: none;
-    margin: 0;
-    padding: 0;
+    margin-left: 0;
+    margin-bottom: 0;
 `
 
 const MenuTab = styled.li`
     float: left;
+    padding: 1em;
+    :first-child{
+        padding-left: 0;
+    }
 `
 
 const MenuButton = styled.button`
     display: block;
     color: black;
     text-align: center;
-    padding: 14px 16px;
     text-decoration: underline;
     border: none;
+    outline: none;
 
     :hover{
         color: grey;
@@ -44,14 +50,18 @@ const Work = () => {
     }
     
     return(
-        <MenuSection> 
-            <MenuBar>
-                <MenuTab id='0'><MenuButton id='0' onClick={event => handleClick(event)}><h3 id='0'>software engineering intern</h3></MenuButton></MenuTab>
-                <MenuTab id='1'><MenuButton id='1' onClick={event => handleClick(event)}><h3 id='1'>salesforce developer</h3></MenuButton></MenuTab>
-                <MenuTab id='2'><MenuButton id='2' onClick={event => handleClick(event)}><h3 id='2'>ml research assistant</h3></MenuButton></MenuTab>
-            </MenuBar>
-            <h1>{value}</h1>
-        </MenuSection>
+        <div>
+            <MenuContainer>
+                <MenuBar>
+                    <MenuTab id='0'><MenuButton id='0' onClick={event => handleClick(event)}><h3 id='0'>software engineering intern</h3></MenuButton></MenuTab>
+                    <MenuTab id='1'><MenuButton id='1' onClick={event => handleClick(event)}><h3 id='1'>salesforce developer</h3></MenuButton></MenuTab>
+                    <MenuTab id='2'><MenuButton id='2' onClick={event => handleClick(event)}><h3 id='2'>ml research assistant</h3></MenuButton></MenuTab>
+                </MenuBar>
+            </MenuContainer>
+            <Card>
+                <h1>{value}</h1>
+            </Card>
+        </div>
     )
     
 }
